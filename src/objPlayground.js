9383721dfbd5a1c6aa0c2e4778694068617f7730
leticiaandrade <leticia933@gmail.com>
2022-01-04 12:28:50 -0300

    requisito 7 - calculator

--- src/objPlayground.js
+++ src/objPlayground.js
@@ -33,7 +33,16 @@
   arrayGenerator('entries', { sum: 3, mult: 2, div: 1, sub: 0 }) // [ [ 'sum', 3 ], [ 'mult', 2 ], [ 'div', 1 ], [ 'sub', 0 ] ]
 */
 
-const calculator = (number1, number2) => {};
+const calculator = (number1, number2) => {
+  let myCalculation = { 
+    sum: Math.floor(number1 + number2), 
+    mult: Math.floor(number1 * number2), 
+    div: Math.floor(number1 / number2), 
+    sub: Math.floor(number1 - number2),
+  };
+  return myCalculation;
+};
+console.log(calculator(1, 3));
 
 const arrayGenerator = (type, object) => {};
 
