e79a637a08498eb01c2a78fe41c842cd1697aaac
leticiaandrade <leticia933@gmail.com>
2022-01-05 02:19:40 -0300

    requisito 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -25,16 +25,25 @@ const productDetails = require('../src/productDetails');
   ]
 
 */
+//ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/isArray
+//https://www.w3schools.com/jsref/jsref_slice_array.asp
 
 describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
   it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
     // Teste se productDetails é uma função.
+    //const myProducts = productDetails('Pizza', 'Fries');
+    expect(typeof productDetails).toBe('function');    
     // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('pizza', 'fries'))).toBeTruthy();
     // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('pizza', 'fries').length).toBe(2);
     // Teste se os dois itens dentro do array retornado pela função são objetos.
+    expect(typeof productDetails('pizza', 'fries')[0]).toBe('object');
+    expect(typeof productDetails('pizza', 'fries')[1]).toBe('object');
     // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(productDetails('pizza', 'fries')[0]).not.toBe(productDetails('pizza', 'fries')[1]);
     // Teste se os dois productIds terminam com 123.
+    expect(productDetails('pizza', 'fries')[0].details.productId.slice(-3)).toBe('123');
+    expect(productDetails('pizza', 'fries')[1].details.productId.slice(-3)).toBe('123');
   });
 });
