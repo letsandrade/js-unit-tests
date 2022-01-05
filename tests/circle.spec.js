028930b8b0d0a38c2c62d25d7192294c9e5045b0
leticiaandrade <leticia933@gmail.com>
2022-01-05 01:04:13 -0300

    requisito 4

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -23,16 +23,27 @@ const circle = require('../src/circle');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
+//ref: https://stackoverflow.com/questions/47754777/jest-how-to-test-for-object-keys-and-values
+
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contedos os valores esperados', () => {
-    fail('Teste vazio!');
-    // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle(' ')).toBeUndefined();
+    expect(circle('string')).toBeUndefined();
     // Teste se circle retorna um objeto.
-    // Teste se o objeto retornado tem 3 propriedades.
+    const newCircle = circle(1);
+    expect(typeof newCircle).toBe('object');
+    // Teste se o objeto retornado tem 3 propriedades. 
+    expect(Object.keys(circle(1)).length).toBe(3);
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    expect(circle(2)).toHaveProperty('circumference', 12.56);
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    expect(circle(3).area).toBeCloseTo(28.26);
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3).radius).toBe(3);
+    expect(circle(3).area).toBeCloseTo(28.26);
+    expect(circle(3).circumference).toBeCloseTo(18.84);
   });
 });
